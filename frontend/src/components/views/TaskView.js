00b7dcff34a0d7a5674ed30d8aec4f9a22754f6c
chrislies <clai20707@gmail.com>
2023-05-24 18:29:01 -0400

    implemented feauture that allows navigation to 'single employee view' from 'single task view'

--- frontend/src/components/views/TaskView.js
+++ frontend/src/components/views/TaskView.js
@@ -7,7 +7,7 @@ const TaskView = (props) => {
       <h1>{task.description}</h1>
       <h3>Task Priority: {task.priority}</h3>
       {task.isComplete ? <h3>Task Completed</h3> : <h3>Task Incomplete</h3>}
-      {task.employee ? <h3>{task.employee.firstname + " " + task.employee.lastname}</h3> : <h3>Unassigned</h3>}
+      {task.employee ? <Link to={`/employees/${task.employeeId}`}><h3>{task.employee.firstname + " " + task.employee.lastname}</h3></Link> : <h3>Unassigned</h3>}
       <Link to={`/editTask/${task.id}`}>Edit task information</Link>
       <br />
       <Link to={`/tasks`}>View all tasks</Link>
