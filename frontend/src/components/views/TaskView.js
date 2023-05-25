0f4837e2fdec6d7ff6709a31c73f470c61be5963
chrislies <clai20707@gmail.com>
2023-05-24 22:49:21 -0400

    implemented completion status indicator for 'all task views' and 'task view' pages

--- frontend/src/components/views/TaskView.js
+++ frontend/src/components/views/TaskView.js
@@ -7,7 +7,9 @@ const TaskView = (props) => {
       <div class="taskViewContainer">
         <h1>{task.description}</h1>
         <h3>Task Priority: {task.priority}</h3>
-        {task.isComplete ? <h3>Task Completed</h3> : <h3>Task Incomplete</h3>}
+        <div style={{ backgroundColor: task.isComplete ? "#43e06b" : "#e02222" }}>
+          {task.isComplete ? <h3>Completed</h3> : <h3>Incomplete</h3>}
+        </div>
         {task.employee ? <Link to={`/employees/${task.employeeId}`}><h3 class="employeeInfo">{task.employee.firstname + " " + task.employee.lastname}</h3></Link> : <h3>Unassigned</h3>}
         <Link to={`/editTask/${task.id}`}>Edit task information</Link>
         <br />
