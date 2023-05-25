e013e711149328c77b5794c65a97f509ad4d1834
chrislies <clai20707@gmail.com>
2023-05-24 23:00:16 -0400

    final touches and styling

--- frontend/src/components/views/HomePageView.js
+++ frontend/src/components/views/HomePageView.js
@@ -4,7 +4,7 @@ const HomePageView = () => {
   return (
     <div className="homePageViewContainerParent">
       <div className="homePageViewContainer">
-        <h1>E.T. Manager</h1>
+        <h1 className="appHeader">E.T. Manager</h1>
         <Link to={'/employees'}><button>All Employees</button></Link>
         <Link to={'/tasks'}><button>All Tasks</button></Link>
       </div>
